45b0a11387d3ffd671d6e6190faf05df54ccd4b6
DmytroNianin <108230718+DmytroNianin@users.noreply.github.com>
2023-02-06 17:42:00 +0100

    hw-js-9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  startButton: document.querySelector('.btn.start'),
+  stopButton: document.querySelector('.btn.stop'),
+};
+
+let intervalId = null;
+
+refs.startButton.addEventListener('click', onStartClick);
+refs.stopButton.addEventListener('click', onStopClick);
+
+function onStartClick() {
+  toggleButton();
+  intervalId = setInterval(changBgColor, 1000);
+}
+
+function onStopClick() {
+  toggleButton();
+  clearInterval(intervalId);
+}
+
+function toggleButton() {
+  refs.startButton.toggleAttribute('disabled');
+  refs.stopButton.toggleAttribute('disabled');
+}
+
+function changBgColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
